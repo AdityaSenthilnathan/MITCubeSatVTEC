db820256ce13ef9addfc28829f5fc77c5b660f09
Duckiator <hyunwoolee2020@gmail.com>
2022-03-16 09:00:55 -0700

    Added code for taking a photo

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -3,8 +3,10 @@
 #AUTHOR: Aakash Senthilnathan
 #DATE: 3/15/22
 
+from tkinter.tix import IMAGETEXT
 import libraries
 import time
+from time import sleep
 import os
 import board
 import busio
@@ -16,6 +18,7 @@ setup imu and camera
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 camera = PiCamera()
+camera.start_preview()
 
 #bonus: function for uploading image to Github
 def git_push():
@@ -34,15 +37,23 @@ def git_push():
 
     
 #SET THRESHOLD
-threshold = 100
+threshold = 50
 
 
 #read acceleration
 while True:
     accelX, accelY, accelZ = sensor.acceleration
-
-    #CHECK IF READINGS ARE ABOVE THRESHOLD
+    print("acceleration: ", sensor.acceleration)
+    print("Threshold: ", threshold)
+     #CHECK IF READINGS ARE ABOVE THRESHOLD
+    if threshold < sensor.acceleration:
+        camera.start_preview()
         #PAUSE
+        sleep(5)
+        #take a photo
+        camera.capture(imgname)
+
+   
 
     
         #TAKE/SAVE/UPLOAD A PICTURE 
@@ -51,9 +62,8 @@ while True:
         if name:
             t = time.strftime("_%H%M%S")      # current time string
             imgname = ('/home/pi/FlatSatChallenge/Images/FileSource/%s%s' % (name,t)) #change directory to your folder
-    
-            #<YOUR CODE GOES HERE>#
-            
-    
-    #PAUSE
 
+            #<YOUR CODE GOES HERE>#
+            git_push()    
+            camera.stop_preview()
+    #PAUSE
