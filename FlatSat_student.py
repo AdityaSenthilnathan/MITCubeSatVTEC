e26d7dd3beb190404bb89a236efe337131351fb7
asent1234 <42815464+asent1234@users.noreply.github.com>
2022-03-16 08:12:37 -0700

    Update FlatSat_student.py

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -17,12 +17,11 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 camera = PiCamera()
 
-"""
 #bonus: function for uploading image to Github
 def git_push():
     try:
         repo = Repo('/home/pi/FlatSatChallenge') #PATH TO YOUR GITHUB REPO
-        repo.git.add('folder path') #PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
+        repo.git.add('Images') #PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
         repo.index.commit('New Photo')
         print('made the commit')
         origin = repo.remote('origin')
@@ -31,7 +30,7 @@ def git_push():
         print('pushed changes')
     except:
         print('Couldn\'t upload to git')
-"""
+
 
     
 #SET THRESHOLD
