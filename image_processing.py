491ac8418be12552bc29136471374d678ff8b7b3
Duckiator <hyunwoolee2020@gmail.com>
2022-05-03 23:13:27 -0700

    had the image processor in terms of hsv

--- image_processing.py
+++ image_processing.py
@@ -2,43 +2,77 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = "turtle.jpg"
-image = cv2.imread(file)
+file = "color.jpg"
 
-rfilt = image[:, :, 2]
-gfilt = image[:, :, 1]
-bfilt = image[:, :, 0]
+img = cv2.imread(file)
+img = cv2.resize(img, (640,680))
 
-rfilt = cv2.boxFilter(image[:,:,2], cv2.CV_32F, (100,100))
-gfilt = cv2.boxFilter(image[:,:,1], cv2.CV_32F, (100,100))
-bfilt = cv2.boxFilter(image[:,:,0], cv2.CV_32F, (100,210))
+plt.imshow(img)
+plt.show()
+
+imhsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+imhsv = cv2.boxFilter(imhsv, -1, (10,10))
+
+h_img = imhsv[:, :, 0]
+s_img = imhsv[:, :, 1]
+v_img = imhsv[:, :, 2]
+
+hfilt = cv2.boxFilter(imhsv[:, :, 0], cv2.CV_32F, (40, 40))
+sfilt = cv2.boxFilter(imhsv[:, :, 1], cv2.CV_32F, (40, 40))
+vfilt = cv2.boxFilter(imhsv[:, :, 2], cv2.CV_32F, (40, 40))
 
 fig, ax = plt.subplots(1,3, figsize = (15, 10))
-ax[2].imshow(rfilt, cmap="Reds")
-ax[1].imshow(gfilt, cmap="Greens")
-ax[0].imshow(bfilt, cmap="Blues")
-plt.show()
 
-mask = cv2.inRange(rfilt, 16.9, 17)
-plt.imshow(mask)
-plt.colorbar()
-plt.imshow
-plt.title("Mask")
-plt.show()
+ax[0].imshow(hfilt, cmap="summer")
+ax[1].imshow(sfilt, cmap="Greens")
+ax[2].imshow(vfilt, cmap="Greens")
 
-sat = cv2.inRange(gfilt, 135, 190)
-plt.imshow(sat)
-plt.colorbar()
-plt.title("Saturation")
+
 plt.show()
 
 
-val = cv2.inRange(bfilt, 40, 80)
-plt.imshow(val)
-plt.colorbar()
-plt.title("Value")
+img_thresh_hue = np.logical_and(imhsv[:,:,0] > -10, imhsv[:,:,0] < 180)
+img_thresh_sat = np.logical_and(imhsv[:,:,1] > 35, imhsv[:,:,1] < 300)
+img_thresh_HS = np.logical_and(img_thresh_hue,img_thresh_sat)
+img_thresh_val = np.logical_and(imhsv[:,:,2] > 100, imhsv[:,:,2] < 300)
+img_thresh_HSV = np.logical_and(img_thresh_HS, img_thresh_val)
+
+imdec = cv2.boxFilter(img_thresh_HSV.astype(int), -1, (10,10), normalize=False)
+
+imdec_threshold = np.argwhere(imdec>1000)
+
+fig, ax = plt.subplots(1,3)
+ax[0].imshow(img_thresh_hue, cmap = "gray")
+ax[1].imshow(img_thresh_sat, cmap = "gray")
+ax[2].imshow(img_thresh_val, cmap = "gray")
+
 plt.show()
 
-res = cv2.bitwise_and(image, image, mask=mask)
-plt.imshow(res)
+plt.imshow(img_thresh_HS, cmap = "gray")
+plt.title("HS")
+
 plt.show()
+
+plt.imshow(img_thresh_HSV, cmap = "gray")
+plt.title("HSV")
+
+plt.show()
+
+plt.imshow(imdec, cmap = "gray")
+plt.title("imdec")
+
+plt.show()
+
+
+pixel_pos = np.average(imdec_threshold, axis = 0)
+print(pixel_pos)
+
+plt.imshow(img)
+plt.imshow(imdec)
+
+plt.plot(pixel_pos[0], pixel_pos[1], "bo")
+plt.show()
+
+ 
+
+
