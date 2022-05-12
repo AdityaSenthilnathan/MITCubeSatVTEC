827bd65c5343f2e4eddcbeb6d0021887928cc57a
Duckiator <hyunwoolee2020@gmail.com>
2022-05-12 13:27:46 -0700

    hi

--- auto_camera.py
+++ auto_camera.py
@@ -16,7 +16,7 @@ sensor2 = adafruit_bno055.BNO055_I2C(i2c)
 camera = PiCamera()
 
 rep_time = 20
-run_time = 60*2
+run_time = 60
 start_time = time.time()
 i = 1 
 tol_t = 0.01
@@ -97,15 +97,14 @@ while run_time > (time.time() - start_time):
 
                 if np.abs(chosen_angle - target_angle) < tol:
                     name = "Chick"
-                    imgname = ('/home/pi/MITCubeSatSatickens/final_project/%s%d' % (name, img_num)) #change directory to your folder   
+                    imgname = ('/home/pi/MITCubeSatSatickens/hi/%s%d' % (name, img_num)) #change directory to your folder   
                     image = camera.capture(imgname+ ".jpg") #take a photo
                     img_num += 1
+                    git_push()
                     break
                 time.sleep(1/refresh_rate)
 
             return image
-    else:
-        git_push()
 
 
 if __name__ == '__main__':
