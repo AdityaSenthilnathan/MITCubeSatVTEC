d9eff5611370da8a2b67270713ed45c48351c909
Duckiator <hyunwoolee2020@gmail.com>
2022-05-11 21:03:38 -0700

    added time

--- auto_camera.py
+++ auto_camera.py
@@ -15,6 +15,12 @@ sensor1 = adafruit_bno055.BNO055_I2C(i2c)
 sensor2 = adafruit_bno055.BNO055_I2C(i2c)
 camera = PiCamera()
 
+rep_time = 20
+run_time = 60*2
+start_time = time.time()
+i = 1 
+tol_t = 0.01
+
 def git_push():
     try:
         repo = Repo('/home/pi/MITCubeSatSatickens') #PATH TO YOUR GITHUB REPO
@@ -28,68 +34,79 @@ def git_push():
     except:
         print('Couldn\'t upload to git')
 
+
 #Code to take a picture at a given offset angle
-def capture(which_angle ='pitch', target_angle = 60, method = "am", tol = 0.5, refresh_rate = 50): #tol is tolerance of the angle
-    #Calibration lines should remain commented out until you implement calibration
-    offset_mag = sc.calibrate_mag()
-    offset_gyro =sc.calibrate_gyro()
-    initial_angle = sc.set_initial(offset_mag)
-    prev_angle = initial_angle
-    print("Begin moving camera.")
-    while True:
-        accelX, accelY, accelZ = sensor1.acceleration #m/s^2
-        magX, magY, magZ = sensor1.magnetic #gauss
-	#Calibrate magnetometer readings
-        magX = magX - offset_mag[0]
-        magY = magY - offset_mag[1]
-        magZ = magZ - offset_mag[2]
+while run_time > (time.time() - start_time):
+    target_time = i*rep_time
+    
+    if abs(target_time-(time.time() - start_time)) < tol_t:
+        def capture(which_angle ='pitch', target_angle = 0, method = "am", tol = 0.5, refresh_rate = 50): #tol is tolerance of the angle
+            #Calibration lines should remain commented out until you implement calibration
+            offset_mag = sc.calibrate_mag()
+            offset_gyro =sc.calibrate_gyro()
+            initial_angle = sc.set_initial(offset_mag)
+            prev_angle = initial_angle
+            print("Begin moving camera.")
 
-        gyroX, gyroY, gyroZ = sensor2.gyro #rad/s
-        #Convert to degrees and calibrate
-        gyroX = gyroX *180/np.pi - offset_gyro[0]
-        gyroY = gyroY *180/np.pi - offset_gyro[1]
-        gyroZ = gyroZ *180/np.pi - offset_gyro[2]
+            img_num = 1
 
-        #TODO: Everything else! Be sure to not take a picture on exactly a
-        #certain angle: give yourself some margin for error. 
-        if method == "am":
-            if which_angle == 'roll':
-                chosen_angle = sc.roll_am(accelX,accelY,accelZ) - prev_angle[0]
-            elif which_angle == 'pitch':
-                chosen_angle = sc.pitch_am(accelX,accelY,accelZ) - prev_angle[1]
-            elif which_angle == "yaw":
-                chosen_angle = sc.yaw_am(accelX,accelY,accelZ) - prev_angle[2]
-            else:
-                print("Invalid Input: Must be roll, pitch, or yaw")
-                image = None
-                break #abort trying to take an image
-                #return None
-        elif method == "gyro":
-            if which_angle == 'roll':
-                chosen_angle = sc.roll_gy(prev_angle[0]) + gyroZ
-            elif which_angle == 'pitch':
-                chosen_angle = sc.pitch_gy(prev_angle[1]) + gyroY
-            elif which_angle == "yaw":
-                chosen_angle = sc.yaw_gy(prev_angle[2]) + gyroX
-            else:
-                print("Invalid Input: Must be roll, pitch, or yaw")
-                image = None
-                break #abort trying to take an image
-                #return None
-        else:
-            print("Invalid Input: Must be am or gyro")
-            image = None
-            break #abort trying to take an image
+            while True:
+                accelX, accelY, accelZ = sensor1.acceleration #m/s^2
+                magX, magY, magZ = sensor1.magnetic #gauss
+            #Calibrate magnetometer readings
+                magX = magX - offset_mag[0]
+                magY = magY - offset_mag[1]
+                magZ = magZ - offset_mag[2]
 
-        if np.abs(chosen_angle - target_angle) < tol:
-            name = "Satickens"
-            t = time.strftime("_%H%M%S")      # current time string
-            imgname = ('/home/pi/MITCubeSatSatickens/Images/%s%s' % (name,t)) #change directory to your folder   
-            image = camera.capture(imgname+ ".jpg") #take a photo
-            git_push()
-            break
-        time.sleep(1/refresh_rate)
+                gyroX, gyroY, gyroZ = sensor2.gyro #rad/s
+                #Convert to degrees and calibrate
+                gyroX = gyroX *180/np.pi - offset_gyro[0]
+                gyroY = gyroY *180/np.pi - offset_gyro[1]
+                gyroZ = gyroZ *180/np.pi - offset_gyro[2]
 
-    return image
+                #TODO: Everything else! Be sure to not take a picture on exactly a
+                #certain angle: give yourself some margin for error. 
+                if method == "am":
+                    if which_angle == 'roll':
+                        chosen_angle = sc.roll_am(accelX,accelY,accelZ) - prev_angle[0]
+                    elif which_angle == 'pitch':
+                        chosen_angle = sc.pitch_am(accelX,accelY,accelZ) - prev_angle[1]
+                    elif which_angle == "yaw":
+                        chosen_angle = sc.yaw_am(accelX,accelY,accelZ) - prev_angle[2]
+                    else:
+                        print("Invalid Input: Must be roll, pitch, or yaw")
+                        image = None
+                        break #abort trying to take an image
+                        #return None
+                elif method == "gyro":
+                    if which_angle == 'roll':
+                        chosen_angle = sc.roll_gy(prev_angle[0]) + gyroZ
+                    elif which_angle == 'pitch':
+                        chosen_angle = sc.pitch_gy(prev_angle[1]) + gyroY
+                    elif which_angle == "yaw":
+                        chosen_angle = sc.yaw_gy(prev_angle[2]) + gyroX
+                    else:
+                        print("Invalid Input: Must be roll, pitch, or yaw")
+                        image = None
+                        break #abort trying to take an image
+                        #return None
+                else:
+                    print("Invalid Input: Must be am or gyro")
+                    image = None
+                    break #abort trying to take an image
+
+                if np.abs(chosen_angle - target_angle) < tol:
+                    name = "Chick"
+                    imgname = ('/home/pi/MITCubeSatSatickens/final_project/%s%d' % (name, img_num)) #change directory to your folder   
+                    image = camera.capture(imgname+ ".jpg") #take a photo
+                    img_num += 1
+                    break
+                time.sleep(1/refresh_rate)
+
+            return image
+    else:
+        git_push()
+
+
 if __name__ == '__main__':
     capture(*sys.argv[1:])
