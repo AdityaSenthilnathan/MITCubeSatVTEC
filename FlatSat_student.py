e907c4dd2fa1d762fb5c50f7f90b4aedbdcd3f26
AdityaSenthilnathan <s.aditya.senthilnathan@gmail.com>
2025-02-06 20:36:18 -0800

    Added a test to see time to upload x number of images

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -19,20 +19,13 @@ from picamera2 import Picamera2  # UPDATED TO USE PICAMERA2
 import numpy as np
 from PIL import Image
 
-print("waiting!")
-sleep(5)
 
 
-# Configuration
-rep_time = 5  # Time interval between photos
-run_time = 15  # Total runtime of the script
-start_time = time.time()
-i = 1  # Photo counter
-tol = 0.01  # Tolerance for timing
 
-# Setup IMU and Camera
-i = 1
-tol = 0.01
+
+
+
+
 
 # setup imu and camera
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -48,19 +41,21 @@ camera.set_controls({'ScalerCrop': [0,0] + full_res})
 # bonus: function for uploading image to Github
 def git_push():
     try:
-        repo = Repo('/home/pi/MITCubeSatVTEC')  # PATH TO YOUR GITHUB REPO
+        repo = Repo('/home/TaftHS/MITCubeSatVTEC')  # PATH TO YOUR GITHUB REPO
         repo.git.add('egg')  # PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
         repo.index.commit('New Photo')
-        print('Made the commit')
+        #print('Made the commit')
         
         # Get the remote origin
         origin = repo.remote('origin')
-        print('added remote')
+        #print('added remote')
         # Push the changes to your branch (adjust the branch name if needed)
-
+        startTime = time.time()
         origin.push()
-        print('pushed changes')
-
+        endTime = time.time()
+        
+        #print('pushed changes')
+        return endTime - startTime
     except Exception as e:
         print(f"Couldn't upload to git: {e}")
         import traceback
@@ -74,34 +69,45 @@ loopPauseTime = 1  # s
 
 
 # Main loop
-print("entering main loop!")
-while run_time > (time.time() - start_time):
-    target_time = i * rep_time
+rep_time = 2 
+def mainLoop(t):
+    # Configuration
+    # Time interval between photos
+    run_time = t # Total runtime of the script
+    start_time = time.time()
+    i = 1  # Photo counter
+    #print("entering main loop!")
+    while run_time > (time.time() - start_time):
+        target_time = i * rep_time
 
-    # TAKE/SAVE/UPLOAD A PICTURE
-    if target_time - (time.time() - start_time) < 0:
-        print("capturing photo")
-        
-        # Name for the photo
-        name = "Chick"  # Last Name, First Initial  ex. FoxJ
-        if name:
-            # Save the photo with a unique name
-            imgname = f'/home/TaftHS/MITCubeSatVTEC/egg/{name}{i}.jpg'  # Change directory to your folder
-            camera.capture_file(imgname)  # UPDATED TO USE PICAMERA2
-            i += 1
-            print("captured photo")
-    # Pause between iterations
-    sleep(loopPauseTime)
+        # TAKE/SAVE/UPLOAD A PICTURE
+        if target_time - (time.time() - start_time) < 0:
+            #print("capturing photo")
+            
+            # Name for the photo
+            name = "Chick"  # Last Name, First Initial  ex. FoxJ
+            if name:
+                # Save the photo with a unique name
+                imgname = f'/home/TaftHS/MITCubeSatVTEC/egg/{name}{i}.jpg'  # Change directory to your folder
+                camera.capture_file(imgname)  # UPDATED TO USE PICAMERA2
+                i += 1
+                #print("captured photo")
+        # Pause between iterations
+        sleep(loopPauseTime)
 
 
-def loadAndCompressImages():
-    for j in range(i):
+def loadAndCompressImages(i):
+    for j in range(int(i)):
         name = "Chick"
         imgname = (f'/home/TaftHS/MITCubeSatVTEC/egg/{name}{j + 1}.jpg')
         image = Image.open(imgname)
         image_resized = image.resize((1920, 1080))
         image_resized.save(imgname)
+def test(num):
+    mainLoop((num * rep_time) + rep_time)
+    loadAndCompressImages((((num * rep_time) + rep_time)/rep_time) -1)
 
-
-loadAndCompressImages()
-git_push()
+    return "img count: " + str(num) + " | Time: " + str(git_push())
+    
+for x in range(1, 10):
+    print(test(x))
