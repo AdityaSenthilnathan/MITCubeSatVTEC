fa3a82547a5a98dd4e8514a7ea205bb521773599
Horriblebarry12 <horriblebarry12@gmail.com>
2025-02-22 17:11:02 -0800

    made some changes

--- Test.py
+++ Test.py
@@ -7,16 +7,12 @@ import board
 import busio
 import adafruit_bno055
 from git import Repo
-from picamera2 import Picamera2  # UPDATED TO USE PICAMERA2
 import numpy as np
 from PIL import Image
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)  # Uncomment if using IMU
-camera = Picamera2()  # UPDATED TO USE PICAMERA2
-camera_config = camera.create_still_configuration()
-camera.configure(camera_config)
-camera.start()
+
 
 while True:
 	accelX, accelY, accelZ = sensor.acceleration  # m/s^2
