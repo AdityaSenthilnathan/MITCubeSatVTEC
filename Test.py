59a092a5f5ed47b143d9a1858b9fe023bc68b67e
AdityaSenthilnathan <s.aditya.senthilnathan@gmail.com>
2025-02-22 17:55:59 -0800

    added the file used in the flat sat so I can copy the code for accelerometer onto my pc

--- Test.py
+++ Test.py
@@ -1,30 +1,29 @@
 import time
 import board
-import busio
-import adafruit_bno055
+import math
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
+from adafruit_lis3mdl import LIS3MDL
+from git import Repo
 
 # Initialize I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = board.I2C()
+accel_gyro = LSM6DS(i2c)
+mag = LIS3MDL(i2c)
 
-# Initialize BNO055 sensor
-sensor = adafruit_bno055.BNO055_I2C(i2c)
+
 
 def read_sensor():
     """Reads and prints data from the BNO055 sensor."""
-    print("Temperature: {}°C".format(sensor.temperature))
-    print("Accelerometer (m/s²): {}".format(sensor.acceleration))
-    print("Magnetometer (µT): {}".format(sensor.magnetic))
-    print("Gyroscope (°/s): {}".format(sensor.gyro))
-    print("Euler angles: {}".format(sensor.euler))
-    print("Quaternion: {}".format(sensor.quaternion))
-    print("Linear Acceleration (m/s²): {}".format(sensor.linear_acceleration))
-    print("Gravity (m/s²): {}".format(sensor.gravity))
+    print("Temperature: {}°C".format(accel_gyro.temperature))
+    print("Accelerometer (m/s²): {}".format(accel_gyro.acceleration))
+    print("Magnetometer (µT): {}".format(mag.magnetic))
+    print("Gyroscope (°/s): {}".format(accel_gyro.gyro))
     print("-" * 40)
 
 try:
     while True:
         read_sensor()
-        time.sleep(1)  # Read data every second
+        time.sleep(.1)  # Read data every second
 
 except KeyboardInterrupt:
     print("Sensor reading stopped.")
