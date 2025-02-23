e48787261d1f0e8a830d77080b39006e187d7660
Horriblebarry12 <horriblebarry12@gmail.com>
2025-02-22 17:23:59 -0800

    made some changes

--- Test.py
+++ Test.py
@@ -1,20 +1,30 @@
-from numpy import sqrt
-import math
 import time
-from time import sleep
-import os
 import board
 import busio
 import adafruit_bno055
-from git import Repo
-import numpy as np
-from PIL import Image
 
+# Initialize I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bno055.BNO055_I2C(i2c)  # Uncomment if using IMU
 
+# Initialize BNO055 sensor
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-while True:
-	accelX, accelY, accelZ = sensor.acceleration  # m/s^2
-	magX, magY, magZ = sensor.magnetic  # gauss
-	print(f"x:{accelX} y:{accelY} z:{accelZ}")
+def read_sensor():
+    """Reads and prints data from the BNO055 sensor."""
+    print("Temperature: {}°C".format(sensor.temperature))
+    print("Accelerometer (m/s²): {}".format(sensor.acceleration))
+    print("Magnetometer (µT): {}".format(sensor.magnetic))
+    print("Gyroscope (°/s): {}".format(sensor.gyro))
+    print("Euler angles: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear Acceleration (m/s²): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s²): {}".format(sensor.gravity))
+    print("-" * 40)
+
+try:
+    while True:
+        read_sensor()
+        time.sleep(1)  # Read data every second
+
+except KeyboardInterrupt:
+    print("Sensor reading stopped.")
